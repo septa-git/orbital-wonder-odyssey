15ba061dcccfe43a2e6751bb4c16805aef885853
septa-git <uchihaitachi.404sa@gmail.com>
2023-11-17 09:26:22 +0700

    development

---
+++ script.js
@@ -0,0 +1,118 @@
+function myFunction() {
+  var element = document.getElementById("sun_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1");
+  element.classList.add("mystyleimg");
+}
+function myFunctionm() {
+  var element = document.getElementById("sun_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1");
+  element.classList.remove("mystyleimg");
+}
+
+function myFunction_Mercury() {
+  var element = document.getElementById("Mercury_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1_Mercury");
+  element.classList.add("mystyleimg_planet");
+}
+function myFunctionm_Mercury() {
+  var element = document.getElementById("Mercury_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1_Mercury");
+  element.classList.remove("mystyleimg_planet");
+}
+
+function myFunction_venus() {
+  var element = document.getElementById("venus_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1_venus");
+  element.classList.add("mystyleimg_planet");
+}
+function myFunctionm_venus() {
+  var element = document.getElementById("venus_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1_venus");
+  element.classList.remove("mystyleimg_planet");
+}
+
+function myFunction_mars() {
+  var element = document.getElementById("mars_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1_mars");
+  element.classList.add("mystyleimg_planet");
+}
+function myFunctionm_mars() {
+  var element = document.getElementById("mars_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1_mars");
+  element.classList.remove("mystyleimg_planet");
+}
+
+function myFunction_earth() {
+  var element = document.getElementById("earth_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1_planet");
+  element.classList.add("mystyleimg_planet");
+  var element = document.getElementById("img1_moon");
+  element.classList.add("mystyleimg_moon");
+}
+function myFunctionm_earth() {
+  var element = document.getElementById("earth_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1_planet");
+  element.classList.remove("mystyleimg_planet");
+}
+
+function myFunction_satrun() {
+  var element = document.getElementById("Saturn_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1_satrun");
+  element.classList.add("mystyleimg_planet");
+}
+function myFunctionm_satrun() {
+  var element = document.getElementById("Saturn_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1_satrun");
+  element.classList.remove("mystyleimg_planet");
+}
+
+function myFunction_Jupiter() {
+  var element = document.getElementById("Jupiter_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1_Jupiter");
+  element.classList.add("mystyleimg_planet");
+}
+function myFunctionm_Jupiter() {
+  var element = document.getElementById("Jupiter_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1_Jupiter");
+  element.classList.remove("mystyleimg_planet");
+}
+
+function myFunction_Uranus() {
+  var element = document.getElementById("Uranus_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1_Uranus");
+  element.classList.add("mystyleimg_planet");
+}
+function myFunctionm_Uranus() {
+  var element = document.getElementById("Uranus_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1_Uranus");
+  element.classList.remove("mystyleimg_planet");
+}
+
+function myFunction_Neptune() {
+  var element = document.getElementById("Neptune_info");
+  element.classList.add("mystyle");
+  var element = document.getElementById("img1_Neptune");
+  element.classList.add("mystyleimg_planet");
+}
+function myFunctionm_Neptune() {
+  var element = document.getElementById("Neptune_info");
+  element.classList.remove("mystyle");
+  var element = document.getElementById("img1_Neptune");
+  element.classList.remove("mystyleimg_planet");
+}
